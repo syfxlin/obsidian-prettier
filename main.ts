5f3ac462c5022bdcdcbbca1d900b125416318d9c
syfxlin <syfxlin@gmail.com>
2021-01-16 16:19:15 +0800

    :tada: init: Init

--- main.ts
+++ main.ts
@@ -1,112 +1,146 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  MarkdownView,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+} from "obsidian";
+import { format } from "prettier/standalone";
+// @ts-ignore
+import markdownParser from "prettier/parser-markdown";
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PrettierSettings {
+  options: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PrettierSettings = {
+  options: "{}",
+};
+
+export default class PrettierPlugin extends Plugin {
+  settings: PrettierSettings;
+
+  async onload() {
+    console.log("Loading Prettier");
+
+    await this.loadSettings();
+
+    this.addRibbonIcon("dice", "Run Prettier All", async () => {
+      await this.runPrettierAll();
+    });
+
+    this.addSettingTab(new PrettierSettingTab(this.app, this));
+
+    this.addCommand({
+      id: "prettier-run",
+      name: "Run",
+      callback: () => this.runPrettier(),
+      hotkeys: [
+        {
+          modifiers: ["Mod", "Alt"],
+          key: "l",
+        },
+      ],
+    });
+  }
+
+  onunload() {
+    console.log("Unloading Prettier");
+  }
+
+  runPrettier() {
+    const view = this.app.workspace.activeLeaf.view;
+    if (view instanceof MarkdownView) {
+      // Do work here
+      const editor = view.sourceMode.cmEditor;
+
+      // Remember the cursor
+      const cursor = editor.getCursor();
+
+      editor.execCommand("selectAll");
+      let text = editor.getSelection();
+
+      try {
+        const formatted = this.runFormat(text);
+        new Notice("Prettier: formatted");
+        editor.replaceSelection(formatted, "start");
+        editor.setCursor(cursor);
+      } catch (e) {
+        console.error(e);
+        if (e.message) {
+          new Notice(e.message);
+        }
+      }
+    }
+  }
+
+  runFormat(text: string) {
+    return format(text, {
+      ...JSON.parse(this.settings.options || "{}"),
+      parser: "markdown",
+      plugins: [markdownParser],
+    });
+  }
+
+  async runPrettierAll() {
+    for (const markdownFile of this.app.vault.getMarkdownFiles()) {
+      const content = await this.app.vault.read(markdownFile);
+      const formatted = this.runFormat(content);
+      await this.app.vault.modify(markdownFile, formatted);
+    }
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class PrettierSettingTab extends PluginSettingTab {
+  plugin: PrettierPlugin;
 
-	async onload() {
-		console.log('loading plugin');
+  constructor(app: App, plugin: PrettierPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		await this.loadSettings();
+  display(): void {
+    let { containerEl } = this;
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+    containerEl.empty();
 
-		this.addStatusBarItem().setText('Status Bar Text');
+    containerEl.createEl("h2", { text: "Prettier Settings" });
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+    new Setting(containerEl)
+      .setName("Prettier Options")
+      .setDesc(this.newOptionsDoc())
+      .addTextArea((text) => {
+        text
+          .setValue(this.plugin.settings.options || "{}")
+          .onChange(async (value) => {
+            this.plugin.settings.options = value;
+            try {
+              JSON.parse(this.plugin.settings.options);
+              await this.plugin.saveSettings();
+            } catch (e) {}
+          });
+        text.inputEl.rows = 8;
+        text.inputEl.cols = 40;
+      });
+  }
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  private newOptionsDoc(): DocumentFragment {
+    const descEl = document.createDocumentFragment();
+    descEl.appendText("See: ");
+    const a = document.createElement("a");
+    a.href = "https://prettier.io/docs/en/options.html";
+    a.text = "Prettier Options";
+    a.target = "_blank";
+    descEl.appendChild(a);
+    return descEl;
+  }
 }
